670af848cffab7cdbe06c9d58042aac04ab12a27
Binay <binaydas1123@gmail.com>
2024-11-02 17:59:07 +0530

    Data re-initialised with a owner property for each listing

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB = async() => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((ob) => ({...ob, owner: "66b67400261c107bac22c176"}));
     await Listing.insertMany(initData.data);
 
     console.log(`Data was initialised`);
