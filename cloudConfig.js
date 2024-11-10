ad883666bedfa4e6c86217ceb46fa4976269193d
Binay <binaydas1123@gmail.com>
2024-11-10 20:07:02 +0530

    Configuration for cloud upload

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 cloudinary.config({
@@ -8,15 +8,15 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
-// const storage = new CloudinaryStorage({
-//     cloudinary: cloudinary,
-//     params: {
-//         folder: 'wanderlust_development',
-//         allowedFormats: ['png', 'jpg', 'jpeg'],
-//     },
-// });
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'wanderlust_development',
+        allowedFormats: ['png', 'jpg', 'jpeg'],
+    },
+});
 
 module.exports = {
     cloudinary, 
-    // storage
+    storage
 }
