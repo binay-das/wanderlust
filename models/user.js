a2ec9cba0185e53a8d680ebbde84c5eb42a96d43
Binay <binaydas1123@gmail.com>
2024-11-10 16:53:13 +0530

    Comments added

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-
+    
 });
 
 userSchema.plugin(passportLocalMongoose);
--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -3,3 +3,5 @@ module.exports = (fn) => {
         fn(req, res, next).catch(next);
     }
 }
+
+// When fn(req, res, next) is called, it returns a Promise. If this Promise rejects (throws an error), .catch(next) catches the error and passes it to next, which triggers Express's error-handling middleware.
