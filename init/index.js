78074d426205c1c72a13909604c82ff1dd73435b
Binay <binaydas1123@gmail.com>
2024-11-27 15:56:13 +0530

    Data reinitialised according to new schema

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const initData = require('./data1.js');
 const Listing = require("../models/listing.js");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URI = process.env.MONGO_URI;
 main()
     .then(() => {
         console.log(`Connected to DB`);
@@ -11,12 +13,12 @@ main()
         console.log(err);
     })
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
 }
 
 const initDB = async() => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((ob) => ({...ob, owner: "672f5404a0b7dbebd681cdd1"}));
+    initData.data = initData.data.map((ob) => ({...ob, owner: "674483580cfb14910d0e1cfa"}));
     await Listing.insertMany(initData.data);
 
     // console.log(initData);
